78bbba292d0655ec35b5afb0e299a8c6f32efc2b
cliffg-softwarelibre <cliffg-softwarelibre@users.noreply.github.com>
2025-04-05 00:27:54 +0000

    deploy: 9b684be70845ee628491608f6966fc2df24ce508

--- search/all_1.js
+++ search/all_1.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['binary_20serialuze_20classes_20and_20functions_20for_20binary_20data_20serialization_0',['Binary Serialuze, Classes and Functions For Binary Data Serialization',['../index.html',1,'']]],
-  ['binary_5fserialize_5fexample_2ecpp_1',['binary_serialize_example.cpp',['../binary__serialize__example_8cpp.html',1,'']]],
-  ['binary_5fserialize_5ftest_2ecpp_2',['binary_serialize_test.cpp',['../binary__serialize__test_8cpp.html',1,'']]],
-  ['byteswap_3',['byteswap',['../byteswap_8hpp.html#a5b101f5f592b8ad4710aa617a91caf3c',1,'chops']]],
-  ['byteswap_2ehpp_4',['byteswap.hpp',['../byteswap_8hpp.html',1,'']]],
-  ['byteswap_5ftest_2ecpp_5',['byteswap_test.cpp',['../byteswap__test_8cpp.html',1,'']]]
+  ['binary_20data_20serialization_0',['Binary Serialuze, Classes and Functions For Binary Data Serialization',['../index.html',1,'']]],
+  ['binary_20serialuze_20classes_20and_20functions_20for_20binary_20data_20serialization_1',['Binary Serialuze, Classes and Functions For Binary Data Serialization',['../index.html',1,'']]],
+  ['binary_5fserialize_5fexample_2ecpp_2',['binary_serialize_example.cpp',['../binary__serialize__example_8cpp.html',1,'']]],
+  ['binary_5fserialize_5ftest_2ecpp_3',['binary_serialize_test.cpp',['../binary__serialize__test_8cpp.html',1,'']]],
+  ['byteswap_4',['byteswap',['../byteswap_8hpp.html#a5b101f5f592b8ad4710aa617a91caf3c',1,'chops']]],
+  ['byteswap_2ehpp_5',['byteswap.hpp',['../byteswap_8hpp.html',1,'']]],
+  ['byteswap_5ftest_2ecpp_6',['byteswap_test.cpp',['../byteswap__test_8cpp.html',1,'']]]
 ];
--- search/pages_1.js
+++ search/pages_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['binary_20serialuze_20classes_20and_20functions_20for_20binary_20data_20serialization_0',['Binary Serialuze, Classes and Functions For Binary Data Serialization',['../index.html',1,'']]]
+  ['binary_20data_20serialization_0',['Binary Serialuze, Classes and Functions For Binary Data Serialization',['../index.html',1,'']]],
+  ['binary_20serialuze_20classes_20and_20functions_20for_20binary_20data_20serialization_1',['Binary Serialuze, Classes and Functions For Binary Data Serialization',['../index.html',1,'']]]
 ];
